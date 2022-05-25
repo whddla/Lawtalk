1dab887a40a2332fcdaad56c8da006e91ad7da14
tamacoding <dnrwhddla300@naver.com>
2022-05-26 02:50:39 +0900

    수정

---
+++ src/com/lawknow/UserreviewVO.java
@@ -0,0 +1,12 @@
+package com.lawknow;
+
+import com.lawyer.action.Action;
+import com.lawyer.action.ActionInfo;
+
+public class UserreviewVO implements Action{
+	
+	ActionInfo actionInfo = new ActionInfo();
+	
+	UserreviewVO userreviewVO = new UserreviewVO();
+	User
+}
