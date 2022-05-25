bef0f7b2f1638624687eba275af49861f4ee2185
KimGunWoo9595 <medneter@naver.com>
2022-05-26 01:16:39 +0900

    추가

--- src/com/lawyer/LawyerFrontController.java
+++ src/com/lawyer/LawyerFrontController.java
@@ -1,5 +1,60 @@
 package com.lawyer;
 
-public class LawyerFrontController {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.member.MemberJoinOk;
+import com.member.action.ActionInfo;
+
+public class LawyerFrontController extends HttpServlet{
+
+	@Override
+	   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	      doProcess(req, resp);
+	   }
+	   
+	   @Override
+	   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	      doProcess(req, resp);
+	   }
+
+	   protected void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	      String requestURL = req.getRequestURI();
+	      String command = requestURL.substring(requestURL.lastIndexOf("/") + 1);
+	      /*ActionInfo actionInfo = null;*/
+	      
+	      if(command.equals("UserJoinOk.me")) {
+	        /* actionInfo = new MemberJoinOk().execute(req, resp);*/
+	         
+	      } else if(command.equals("UserJoin.me")) {
+	         /*actionInfo = new ActionInfo();
+	         actionInfo.setRedirect(true);
+	         actionInfo.setPath(req.getContextPath() + "/join.jsp");*/
+	      }else if(command.equals("LawyerJoinOk.me")) {
+	    	  
+	      }else if(command.equals("LawyerJoin.me")) {
+	    	  
+	      }
+	      
+	      
+	      else {
+	         // 404 일 때 출력할 에러 페이지 경로 작성
+	      }
+	      
+	      
+	      if(actionInfo != null) {
+	         if(actionInfo.isRedirect()) {
+	            resp.sendRedirect(actionInfo.getPath());
+	         }else {
+	            RequestDispatcher dispatcher = req.getRequestDispatcher(actionInfo.getPath());
+	            dispatcher.forward(req, resp);
+	         }
+	      }
+	      
+	   }
 }
