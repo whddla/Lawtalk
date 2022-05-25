093e649a0474c26a8a137e1a13a90a9fdd62db76
jaeweon <fhfkfk789@naver.com>
2022-05-26 01:38:00 +0900

    vo 입니다

---
+++ src/com/lawyercomment/domain/vo/LawyercommentVO.java
@@ -0,0 +1,62 @@
+package com.lawyercomment.domain.vo;
+
+public class LawyercommentVO {
+		private int lawyerWriteNum;
+		private int lawyerNum;
+		private int requestNum;
+		private String lawyerTitle;
+		private String lawyerContent;
+		private String lawyerWriteDate;
+
+		public LawyercommentVO() {;}
+
+		public int getLawyerWriteNum() {
+			return lawyerWriteNum;
+		}
+
+		public void setLawyerWriteNum(int lawyerWriteNum) {
+			this.lawyerWriteNum = lawyerWriteNum;
+		}
+
+		public int getLawyerNum() {
+			return lawyerNum;
+		}
+
+		public void setLawyerNum(int lawyerNum) {
+			this.lawyerNum = lawyerNum;
+		}
+
+		public int getRequestNum() {
+			return requestNum;
+		}
+
+		public void setRequestNum(int requestNum) {
+			this.requestNum = requestNum;
+		}
+
+		public String getLawyerTitle() {
+			return lawyerTitle;
+		}
+
+		public void setLawyerTitle(String lawyerTitle) {
+			this.lawyerTitle = lawyerTitle;
+		}
+
+		public String getLawyerContent() {
+			return lawyerContent;
+		}
+
+		public void setLawyerContent(String lawyerContent) {
+			this.lawyerContent = lawyerContent;
+		}
+
+		public String getLawyerWriteDate() {
+			return lawyerWriteDate;
+		}
+
+		public void setLawyerWriteDate(String lawyerWriteDate) {
+			this.lawyerWriteDate = lawyerWriteDate;
+		}
+		
+		
+}
---
+++ src/com/user/domain/vo/UserVO.java
@@ -0,0 +1,124 @@
+package com.user.domain.vo;
+
+public class UserVO {
+
+	private int userNum;
+	private String userId;
+	private String lawyerName;
+	private String lawyerEmail;
+	private String lawyerPw;
+	private String lawyerPhoneNum;
+	private String lawyerGender;
+	private int lawyerBirth;
+	private String lawyerKakaoEmail;
+	private String lawyerFacebookEmail;
+	
+	
+	public UserVO() {;}
+
+
+	public int getUserNum() {
+		return userNum;
+	}
+
+
+	public void setUserNum(int userNum) {
+		this.userNum = userNum;
+	}
+
+
+	public String getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
+	public String getLawyerName() {
+		return lawyerName;
+	}
+
+
+	public void setLawyerName(String lawyerName) {
+		this.lawyerName = lawyerName;
+	}
+
+
+	public String getLawyerEmail() {
+		return lawyerEmail;
+	}
+
+
+	public void setLawyerEmail(String lawyerEmail) {
+		this.lawyerEmail = lawyerEmail;
+	}
+
+
+	public String getLawyerPw() {
+		return lawyerPw;
+	}
+
+
+	public void setLawyerPw(String lawyerPw) {
+		this.lawyerPw = lawyerPw;
+	}
+
+
+	public String getLawyerPhoneNum() {
+		return lawyerPhoneNum;
+	}
+
+
+	public void setLawyerPhoneNum(String lawyerPhoneNum) {
+		this.lawyerPhoneNum = lawyerPhoneNum;
+	}
+
+
+	public String getLawyerGender() {
+		return lawyerGender;
+	}
+
+
+	public void setLawyerGender(String lawyerGender) {
+		this.lawyerGender = lawyerGender;
+	}
+
+
+	public int getLawyerBirth() {
+		return lawyerBirth;
+	}
+
+
+	public void setLawyerBirth(int lawyerBirth) {
+		this.lawyerBirth = lawyerBirth;
+	}
+
+
+	public String getLawyerKakaoEmail() {
+		return lawyerKakaoEmail;
+	}
+
+
+	public void setLawyerKakaoEmail(String lawyerKakaoEmail) {
+		this.lawyerKakaoEmail = lawyerKakaoEmail;
+	}
+
+
+	public String getLawyerFacebookEmail() {
+		return lawyerFacebookEmail;
+	}
+
+
+	public void setLawyerFacebookEmail(String lawyerFacebookEmail) {
+		this.lawyerFacebookEmail = lawyerFacebookEmail;
+	}
+
+
+	
+	
+	
+	
+}
