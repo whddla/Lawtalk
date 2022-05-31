463fd360f7232d1d4e3ff68fb39b49c0d46f6d65
whddla <whddla8912@gmail.com>
2022-05-31 21:20:57 +0900

    회원가입 수정, 로그인 저장 만드는중

---
+++ src/com/lawknow/userRegister/UserLogin.java
@@ -0,0 +1,54 @@
+package com.lawknow.userRegister;
+
+import java.io.IOException;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.lawyer.action.ActionInfo;
+
+public class UserLogin {
+
+	  public ActionInfo execute(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		  ActionInfo actionInfo = new ActionInfo();
+    
+    //쿠키 검사
+    String userId = null;
+    String userPw = null;
+    String autoLogin = null;
+    
+    String cookieCheck = req.getHeader("Cookie");
+    
+    
+    if(cookieCheck != null) {
+       Cookie[] cookies = req.getCookies();
+       for(Cookie cookie : cookies) {
+          if(cookie.getName().equals("userId")) {
+        	  userId = cookie.getValue();
+             
+          } else if(cookie.getName().equals("userPw")) {
+        	  userPw = cookie.getValue();
+             
+          }else if(cookie.getName().equals("autoLogin")) {
+             autoLogin = cookie.getValue();
+             
+          }
+       }
+    }
+    
+    req.setAttribute("userId", userId);
+    req.setAttribute("userPw", userPw);
+    req.setAttribute("autoLogin", autoLogin);
+    
+    System.out.println(userId);
+    System.out.println(userPw);
+    System.out.println(autoLogin);
+    
+    actionInfo.setPath("/kovengerss/login.jsp");
+    actionInfo.setRedirect(false);
+    
+    return actionInfo;
+ }
+
+}
