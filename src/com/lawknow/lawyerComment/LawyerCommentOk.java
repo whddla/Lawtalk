f833f237f856833a78f867e201708858fe60fa81
jaeweon <fhfkfk789@naver.com>
2022-06-07 00:09:10 +0900

    이재원

--- src/com/lawknow/lawyerComment/LawyerCommentOk.java
+++ src/com/lawknow/lawyerComment/LawyerCommentOk.java
@@ -6,7 +6,10 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+
 import com.lawknow.domain.dao.LawyerCommentDAO;
+import com.lawknow.domain.dao.LawyerDAO;
 import com.lawknow.domain.vo.LawyerCommentVO;
 import com.lawyer.action.Action;
 import com.lawyer.action.ActionInfo;
@@ -19,27 +22,33 @@ public class LawyerCommentOk implements Action{
 		req.setCharacterEncoding("UTF-8");
 		LawyerCommentVO lawyerCommentVO = new LawyerCommentVO();
 		LawyerCommentDAO lawyerCommentDAO = new LawyerCommentDAO();
-	
-		
+		LawyerDAO lawyerDAO = new LawyerDAO();
+        int lawyerNum = (int)req.getSession().getAttribute("lawyerNum");
+        
+        String lawyerName = lawyerDAO.getLawyerName(lawyerNum);
+        List<LawyerCommentVO> comments = lawyerCommentDAO.getCommentList(lawyerName);
+        System.out.println("변호사 이름 : " + lawyerName);
+    	comments.forEach(comment -> {
+			comment.getLawyerWriteNum();
+			System.out.println("list lawyer writeNum" + comment.getLawyerWriteNum());
+		});
+        
+        
+        
+        
 		lawyerCommentVO.setContent(req.getParameter("content"));
 		System.out.println("commentOk에서 content 받아옴");
 		
 		lawyerCommentVO.setLawyerNum((int)req.getSession().getAttribute("lawyerNum"));
 		System.out.println("commentOk에서 lawyerNum 받아옴");
-//		lawyerCommentVO.setLawyerWriteNum((int)req.getSession().getAttribute("lawyerNum"));
-//		System.out.println("commentOk에서 LawyerWriteNum 받아옴" + (int)req.getSession().getAttribute("lawyerWriteNum"));
 		System.out.println((int)req.getSession().getAttribute("lawyerNum"));
 		lawyerCommentVO.setLawyerName((String)req.getSession().getAttribute("lawyerName"));
 		System.out.println("commentOk에서 lawyername 받아옴");
 		
-		
-
+	
 		lawyerCommentDAO.commentWrite(lawyerCommentVO);
 		System.out.println("1번 째 작성" + lawyerCommentVO.getLawyerWriteNum());
 
 		return null;
-		
 	}
-		
-		
 }
